7decc7f193c036e3411bf4dec2065bbe71b0a34c
Nikhil Vimal <techvoltz@gmail.com>
2015-03-14 15:03:07 -0500

    Fixing a lot!

--- chuck-norris-api.php
+++ chuck-norris-api.php
@@ -24,19 +24,9 @@ Class Chuck_Norris_Jokes {
 
 	}
 
-	// The shortcode
-	public function chuck_norris_shortcode() {
+	public function chuck_norris_function() {
+		if (! $joke = get_transient('chuck_norris') ) {
 
-		//The best transient
-		$key = 'chuck_norris';
-
-		// Let's see if we have a cached version
-		$chuck_norris_transient = get_transient($key);
-		if ($chuck_norris_transient !== false) {
-			return $chuck_norris_transient;
-		}
-
-		else {
 			// If there's no cached version, let's get a joke
 			$jsonurl     = "http://api.icndb.com/jokes/random?limitTo=[nerdy]";
 			$json        = wp_remote_get( $jsonurl );
@@ -48,14 +38,22 @@ Class Chuck_Norris_Jokes {
 			else {
 				// If everything's okay, parse the body and json_decode it
 				$json_output = json_decode( wp_remote_retrieve_body( $json ));
-				$joke        = $json_output->value->joke;
+				$joke = $json_output->value->joke;
 
 				// Store the result in a transient, expires after 1 day
 				// Also store it as the last successful using update_option
-				set_transient( $key, $joke, 60*1 );
+				if ($json_output->type = "success") {
+
+					set_transient( 'chuck_norris', $joke, 60 * 1 );
+				}
 			}
 		}
-		echo esc_html($joke);
+			echo esc_html($joke);
+	}
+
+	// The shortcode
+	public function chuck_norris_shortcode() {
+		return $this->chuck_norris_function();
 	}
 
 	/**
@@ -74,21 +72,11 @@ Class Chuck_Norris_Jokes {
 
 	public function roundhouse_widget_function() {
 
-		$jsonurl = "http://api.icndb.com/jokes/random?limitTo=[nerdy]";
-		$json = wp_remote_get($jsonurl);
-		if ( is_wp_error( $json ) ) {
-			return "Chuck Norris accidentally kicked the server, it will be up soon!";
-		} else {
-		$body = wp_remote_retrieve_body( $json );
-		$json_output = json_decode( $body );
-		
-		if ($json_output->type = "success") {
-			echo esc_html($json_output->value->joke);
-		}
+		return $this->chuck_norris_function();
 
 		echo '<p><strong>Refresh Page for another great Chuck Norris Joke</strong></p>';
-		}
+
 	}
+
 } //The end? It's only the beginning...
 new Chuck_Norris_Jokes();
-
